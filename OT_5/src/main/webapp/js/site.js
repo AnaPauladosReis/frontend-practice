e853c250f7312883bc13ddd5b1cda8761d951b6e
Ana Paula dos Reis <anareis87@icloud.com>
2024-10-03 21:43:13 -0300

    fim js

--- OT_5/src/main/webapp/js/site.js
+++ OT_5/src/main/webapp/js/site.js
@@ -1,2 +1,39 @@
-alert("Achou!");
+function validaFaleConosco() {
+    
+    if (document.frmfaleconosco.txtnome.value === "") {
+      alert("Preencha o campo Nome.");
+      document.frmfaleconosco.txtnome.focus();
+      return false;
+    }
+  
+  
+    if (document.frmfaleconosco.txtfone.value === "") {
+      alert("Preencha o campo Telefone.");
+      document.frmfaleconosco.txtfone.focus();
+      return false;
+    }
+  
 
+    if (document.frmfaleconosco.txtemail.value === "") {
+      alert("Preencha o campo E-mail.");
+      document.frmfaleconosco.txtemail.focus();
+      return false;
+    }
+  
+  
+     if (document.frmfaleconosco.motivo.value === "") {
+      alert("Selecione um motivo.");
+       document.frmfaleconosco.motivo.focus();
+       return false;
+    }
+  
+  
+     if (document.frmfaleconosco.txacomentario.value === "") {
+       alert("Preencha o campo Comentário.");
+       document.frmfaleconosco.txacomentario.focus();
+       return false;  
+     }
+  
+    
+    return true;
+  }
