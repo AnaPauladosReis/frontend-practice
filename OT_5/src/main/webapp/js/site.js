2969aaf3c293cdad3a42a85f2ebe3d68b86ea47a
Ana Paula dos Reis <anareis87@icloud.com>
2024-10-07 21:02:41 -0300

    atualizações dia 07 de Outubro

--- OT_5/src/main/webapp/js/site.js
+++ OT_5/src/main/webapp/js/site.js
@@ -1,39 +1,81 @@
 function validaFaleConosco() {
-    
-    if (document.frmfaleconosco.txtnome.value === "") {
-      alert("Preencha o campo Nome.");
-      document.frmfaleconosco.txtnome.focus();
-      return false;
-    }
-  
-  
-    if (document.frmfaleconosco.txtfone.value === "") {
-      alert("Preencha o campo Telefone.");
-      document.frmfaleconosco.txtfone.focus();
-      return false;
-    }
-  
+  // Obter os elementos do formulário
+  var nome = document.frmfaleconosco.txtnome;
+  var fone = document.frmfaleconosco.txtfone;
+  var email = document.frmfaleconosco.txtemail;
+  var motivo = document.frmfaleconosco.selmotivo;
+  var comentario = document.frmfaleconosco.comentario;
+  var produto = document.frmfaleconosco.txtproduto; 
 
-    if (document.frmfaleconosco.txtemail.value === "") {
-      alert("Preencha o campo E-mail.");
-      document.frmfaleconosco.txtemail.focus();
-      return false;
-    }
-  
-  
-     if (document.frmfaleconosco.motivo.value === "") {
-      alert("Selecione um motivo.");
-       document.frmfaleconosco.motivo.focus();
-       return false;
-    }
-  
-  
-     if (document.frmfaleconosco.txacomentario.value === "") {
-       alert("Preencha o campo Comentário.");
-       document.frmfaleconosco.txacomentario.focus();
-       return false;  
-     }
-  
-    
-    return true;
+  // Validações
+  if (nome.value === "") {
+    alert("Preencha o campo Nome.");
+    nome.focus();
+    return false;
   }
+
+  if (fone.value === "") {
+    alert("Preencha o campo Telefone.");
+    fone.focus();
+    return false;
+  }
+
+  if (email.value === "" || !/\S+@\S+\.\S+/.test(email.value)) {
+    alert("Preencha o campo E-mail com um endereço válido.");
+    email.focus();
+    return false;
+  }
+
+  if (motivo.value === "") {
+    alert("Selecione um motivo.");
+    motivo.focus();
+    return false;
+  }
+
+  // ✨ Validação do campo Produto: ✨ 
+  if (motivo.value === "PR" && produto.value === "") {
+    alert("Preencha o campo Produto.");
+    produto.focus();
+    return false;
+  }
+
+  if (comentario.value === "") {
+    alert("Preencha o campo Comentário.");
+    comentario.focus();
+    return false;
+  }
+
+  return true;
+}
+
+function verificaMotivo(motivo) {
+  var elemento = document.getElementById("produto"); 
+
+  if (motivo === "PR") {
+    var select = document.createElement("select");
+    select.setAttribute("name", "txtproduto");
+    select.setAttribute("id", "txtproduto");
+
+    var option = document.createElement("option");
+    option.setAttribute("value", "");
+    option.appendChild(document.createTextNode("Escolha"));
+    select.appendChild(option);
+
+    option = document.createElement("option");
+    option.setAttribute("value", "FR");
+    option.appendChild(document.createTextNode("Freezer"));
+    select.appendChild(option);
+
+    option = document.createElement("option");
+    option.setAttribute("value", "GE");
+    option.appendChild(document.createTextNode("Geladeira"));
+    select.appendChild(option);
+
+    elemento.appendChild(select);
+
+  } else {
+    if (elemento.firstChild) {
+      elemento.removeChild(elemento.firstChild);
+    }
+  }
+}
